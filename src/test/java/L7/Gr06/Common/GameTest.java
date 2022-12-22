4383b19cf9fcefaf0fcaf614f7c42a95c194ffe8
David <up202203777@edu.fe.up.pt>
2022-12-22 23:59:28 +0000

    Game Test constructor

---
+++ src/test/java/L7/Gr06/Common/GameTest.java
@@ -0,0 +1,26 @@
+package L7.Gr06.Common;
+
+import L7.Gr06.Arena.*;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameTest {
+    @Test
+    public void gameConstructorTest() throws IOException, URISyntaxException, FontFormatException {
+        Game game = new Game();
+        List<Arena> lst = game.allLevels;
+
+        assertTrue(lst.get(0) instanceof Level1);
+        assertTrue(lst.get(1) instanceof Level2);
+        assertTrue(lst.get(2) instanceof Level3);
+        assertTrue(lst.get(3) instanceof Level4);
+        assertTrue(lst.get(4) instanceof Level5);
+        assertTrue(lst.get(5) instanceof Level6);
+    }
+}
